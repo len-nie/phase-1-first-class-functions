133a9f3bd6979404973c697348e9ea0f90e647ff
Lennie <lennie.mbotela@student.moringaschool.com>
2023-04-08 04:28:43 +0300

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(lennie){
+ return lennie();
+}
+
+function returnsANamedFunction (){
+  return returnsANamedFunction
+}
+
+
+function returnsAnAnonymousFunction() {
+  return function() {
+  };
+}
